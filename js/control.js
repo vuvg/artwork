e64b6231b48736bf0f18c2a5f88f23cf728e0a69
vuvg <vogiavu@gmail.com>
2021-10-17 15:40:59 +0700

    Sunday Working

---
+++ js/control.js
@@ -0,0 +1,22 @@
+window.onload = function() {
+  let musicPlayer = new Audio("music/My Daisy.mp3");
+  musicPlayer.setAttribute("autoplay", true);
+  musicPlayer.setAttribute("controls", "controls");
+  musicPlayer.setAttribute("id", "musicPlayer");
+  let musicDiv = document.getElementsByClassName("music")[0];
+  musicDiv.appendChild(musicPlayer);
+}
+
+function showLyric() {
+  let lyric = document.getElementsByClassName("lyric")[0];
+  let showLyricButton = document.getElementsByClassName("showLyric")[0];
+  let isDisplayLyric = getComputedStyle(lyric, null).display;
+  console.log(isDisplayLyric);
+  if (isDisplayLyric == "none") {
+    lyric.style.display = "block";
+    showLyricButton.children[0].innerHTML = "Thu gọn";
+  } else {
+    lyric.style.display = "none";
+    showLyricButton.children[0].innerHTML = "Xem lời";
+  }
+}
